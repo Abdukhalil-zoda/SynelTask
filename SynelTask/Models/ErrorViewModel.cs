a24f6ec2171228978d508de702fc47987f84c302
Abdulaziz <abdu3332099@outlook.com>
2025-03-23 22:55:23 +0500

    Add project files.

---
+++ SynelTask/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SynelTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
